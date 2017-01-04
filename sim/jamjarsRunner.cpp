2e6263e1e30c40d532fce1b4d03841dd99d4ac06
Michael Manto <michaelbmanto@gmail.com>
2017-01-04 17:50:06 +0000

    Deleting works successfully

--- sim/jamjarsRunner.cpp
+++ sim/jamjarsRunner.cpp
@@ -5,6 +5,7 @@
 #define NUMROWS 5
 #define NUMCOLS 5
 #define NUMSYMBOLS 3
+#define MINCLUSTERLENGTH 3
 
 class Rng {
 	unsigned long long initialSeed;
@@ -57,7 +58,7 @@ int printBoard(int (*gameBoard)[NUMCOLS])
 int checkSquare(int (*gameBoard)[NUMCOLS], int (*markBoard)[NUMCOLS], int x, int y)
 {
 	//printf("SQUAURE %i, %i\n", x, y);
-	markBoard[x][y] = gameBoard[x][y];
+	markBoard[x][y] = gameBoard[x][y]; //mark a square once you check it
 
 	int found = 1;
 	int xdelta = 0;
@@ -65,10 +66,10 @@ int checkSquare(int (*gameBoard)[NUMCOLS], int (*markBoard)[NUMCOLS], int x, int
 
 	int deltalist[4][2] = 
 	{
-		{-1, 0},
-		{1, 0},
-		{0, -1},
-		{0, 1}
+		{-1, 0}, //up
+		{1, 0}, //down
+		{0, -1}, //left
+		{0, 1}  //right
 	};
 
 	for (int delta = 0; delta<4; delta++)
@@ -79,13 +80,13 @@ int checkSquare(int (*gameBoard)[NUMCOLS], int (*markBoard)[NUMCOLS], int x, int
 
 		// printf("checking %i, %i\n", x+xdelta, y+ydelta);
 
-		 if (y+ydelta >= 0 && x+xdelta >= 0){ 
-		 	if (markBoard[x+xdelta][y+ydelta] == 0) //check if you can go there
+		 if (y+ydelta >= 0 && x+xdelta >= 0 && y+ydelta < NUMCOLS && x+xdelta < NUMROWS){   //check that where you want to go is in bounds
+		 	if (markBoard[x+xdelta][y+ydelta] == 0) //check you haven't been there before
 		 	{
 		 		//printf("I found %i\n", gameBoard[x+xdelta][y+ydelta]);
-		 		if (gameBoard[x+xdelta][y+ydelta] == gameBoard[x][y]) {
+		 		if (gameBoard[x+xdelta][y+ydelta] == gameBoard[x][y]) {  //if the symbol matches
 		 			//printf ("That's a match, so I'll see if there are any more\n");
-		 			found += checkSquare(gameBoard, markBoard, x+xdelta, y+ydelta); 
+		 			found += checkSquare(gameBoard, markBoard, x+xdelta, y+ydelta);  //go investigate that square to find more and tell me how many you found
 		 		}
 		 		
 
@@ -96,6 +97,45 @@ int checkSquare(int (*gameBoard)[NUMCOLS], int (*markBoard)[NUMCOLS], int x, int
 	}
 
 	return found;
+}
+
+int deleteAndPopulate(int (*gameBoard)[NUMCOLS], int (*clearBoard)[NUMCOLS] )
+{
+	int spotsToFill[NUMCOLS] = {0};
+
+
+	int row = 0;
+	int col =0;
+
+	for (row = 0; row<5;  row++)
+	{
+		col=0;
+		spotsToFill[row] = 0;
+
+		for (col = 0; col < NUMCOLS; col++) {
+
+			if (clearBoard[row][col] == 0)  
+			{
+				gameBoard[row][col-spotsToFill[row]] = gameBoard[row][col];
+			}	else spotsToFill[row]++;
+
+		}
+
+		
+		for (col = NUMCOLS-1; col > NUMCOLS-spotsToFill[row] -1; col--) {	
+			gameBoard[row][col] = 0; //TODO NEXT - Populate
+		}
+
+	}
+	printf ("%i %i %i %i %i ",spotsToFill[0], spotsToFill[1], spotsToFill[2], spotsToFill[3], spotsToFill[4]);
+
+	printf(">>\n");
+
+
+
+
+
+	return 0;
 }
 
 int checkGame(int (*gameBoard)[NUMCOLS])
@@ -109,6 +149,7 @@ int checkGame(int (*gameBoard)[NUMCOLS])
 	int numWins = 0;
 
 
+	//Check all squares
 	for (int x=0; x<NUMCOLS; x++)
 	for (int y=0; y<NUMCOLS; y++)
 	{
@@ -116,7 +157,7 @@ int checkGame(int (*gameBoard)[NUMCOLS])
 		{
 			clusterLength = checkSquare(gameBoard, markBoard, x, y);
 			printf("(%i, %i), Symbol = %i, Number =  %i\n", x, y, gameBoard[x][y],clusterLength);
-			if (clusterLength >4) { 
+			if (clusterLength >= MINCLUSTERLENGTH) { 
 				printf("RECORDED, this is win %i\n",numWins); 
 				 winStarts[numWins][0] = x;  
 				 winStarts[numWins][1] = y; 
@@ -124,21 +165,29 @@ int checkGame(int (*gameBoard)[NUMCOLS])
 				 numWins++;
 				
 			}
-		} else printf("(%i, %i), Skipped\n");
+		} else printf("(%i, %i), Skipped\n");  //don't check the square if it was already looked at
 				
 	}
 
+
+
+	//Mark winning combinations for deletion
 	for (int win = 0; win< numWins; win++)
 	{
 		printf("Win at %i, %i\n", winStarts[win][0], winStarts[win][1]);
 
-		//TODO NEXT Delete the wins. Could try to do this as they are found, or at least mark them as they are found. Then delete the marks. Easy way is just to search from the coordinates given in the win list and delete all the marked items
-
+		checkSquare(gameBoard, clearBoard, winStarts[win][0], winStarts[win][1]);
 	}
 
 	printBoard(clearBoard);
+
+	deleteAndPopulate(gameBoard, clearBoard);
+
+	printBoard(gameBoard);
 	
 }
+
+
 
 
 int createGame(int (*gameBoard)[NUMCOLS], int seed)
