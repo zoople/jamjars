1964d4fc1becfa823e547d291229f311f3a98da6
Michael Manto <michaelbmanto@gmail.com>
2017-01-19 10:49:22 +0000

    Added: Shuffle, Jam collection (inc triggers, trigger amounts, jam collection rates)

--- sim/jamjarsRunner.cpp
+++ sim/jamjarsRunner.cpp
@@ -7,6 +7,10 @@
 #define NUMSYMBOLS 6
 #define MINCLUSTERLENGTH 3
 
+#define STATE_BOUGHTGAME 100
+#define STATE_FREEGAME 200
+
+
 #include "jamjarsPaytable.h"
 
 class Rng {
@@ -144,7 +148,12 @@ int deleteAndPopulate(int (*gameBoard)[NUMCOLS], int (*clearBoard)[NUMCOLS], Rng
 	return 0;
 }
 
-int checkGame(int (*gameBoard)[NUMCOLS], Rng *tileRng)
+int applyFeature( int (*gameBoard)[NUMCOLS], int featureType)
+{
+	printf("FEATURE CALL FOR JAM %i \n", featureType);
+}
+
+int checkGame(int state, int (*gameBoard)[NUMCOLS], int *jam, int *triggers, Rng *tileRng)
 {
 	int markBoard[NUMROWS][NUMCOLS] = {{0}};
 	int clearBoard[NUMROWS][NUMCOLS] = {{0}};
@@ -156,6 +165,20 @@ int checkGame(int (*gameBoard)[NUMCOLS], Rng *tileRng)
 
 	int wins = 0;
 
+	//Apply any special features
+	if (state == STATE_FREEGAME)
+	{
+		if (triggers[0] > 0)
+		{
+			for (int t=1; t<7; t++) {
+				if (triggers[t] >0) applyFeature(  gameBoard, t);
+				triggers[t] = 0;
+			}
+
+			triggers[0] = 0;
+		}
+	}
+
 	//Check all squares
 	for (int x=0; x<NUMCOLS; x++)
 	for (int y=0; y<NUMCOLS; y++)
@@ -165,12 +188,15 @@ int checkGame(int (*gameBoard)[NUMCOLS], Rng *tileRng)
 			clusterLength = checkSquare(gameBoard, markBoard, x, y);
 			printf("(%i, %i), Symbol = %i, Number =  %i\n", x, y, gameBoard[x][y],clusterLength);
 			if (clusterLength >= MINCLUSTERLENGTH) { 
+				jam[ gameBoard[x][y]  ] += jamCollection[gameBoard[x][y]][clusterLength];  //award jam based on the cluser
+				if ( jam[ gameBoard[x][y] ] >= jamTriggers[ gameBoard[x][y] ] ) { triggers[ gameBoard[x][y] ] ++; jam[ gameBoard[x][y]] = 0; triggers[0]++;} //mark triggers as ready if the jam jar is full
 				printf("RECORDED, this is win %i\n",numWins); 
 				 winStarts[numWins][0] = x;  
 				 winStarts[numWins][1] = y; 
 				printf("checking: (%i, %i) %i, %i\n", x,y,winStarts[numWins][0], winStarts[numWins][1]);
 				 numWins++;
 				 wins += paytable[gameBoard[x][y]][0];
+				 printf ("PAY OF %i \n", paytable[gameBoard[x][y]][0]);
 
 				
 			}
@@ -224,6 +250,47 @@ int createGame(int (*gameBoard)[NUMCOLS], Rng *tileRng)
 
 }
 
+int shuffleBoard(int (*gameBoard)[NUMCOLS], Rng *tileRng )
+{
+	int numShuffles = 0;
+
+	tileRng->generateNext();
+	numShuffles = 15 + tileRng->getNext(15);
+	int temp =0;
+
+	printf ("Going to do %i shuffles\n", numShuffles);
+
+	int sA, sB, rA,cA,rB,cB = 0;
+
+	for (int shuffle = 0; shuffle< numShuffles; shuffle++)
+	{
+		tileRng->generateNext();
+		sA = tileRng->getNext(NUMROWS*NUMCOLS);
+
+		rA = sA % NUMROWS;
+		cA = (sA - (sA % NUMROWS) ) / NUMCOLS;
+
+		//printf("%i is %i, %i\n", sA, rA, cA);
+
+		tileRng->generateNext();
+		sB = tileRng->getNext(NUMROWS*NUMCOLS);
+
+		rB = sB % NUMROWS;
+		cB = (sB - (sB % NUMROWS) ) / NUMCOLS;
+
+		//printf("%i is %i, %i\n", sB, rB, cB);
+
+		//printf ("Swap those\n");
+
+		temp = gameBoard[rA][cA];
+		gameBoard[rA][cA] = gameBoard[rB][cB];
+		gameBoard[rB][cB] = temp;
+	}
+
+	printBoard(gameBoard);
+
+}
+
 int main()
 {
 	int gameBoard[NUMROWS][NUMCOLS] = {0};
@@ -231,14 +298,66 @@ int main()
 	tileRng.setSeed(10);
 	int numWins = 0;
 
+	int jam[7] = {0};
+	int jamTriggerSwitch[7] = {0};
+//I int jamTriggers[7] = {0, 20, 20, 20, 20, 20, 20};
+
 	createGame(gameBoard, &tileRng );
 	printBoard(gameBoard);
 
-	numWins=1;
+	shuffleBoard(gameBoard, &tileRng);
+
+	int state = STATE_BOUGHTGAME;
+
+	struct gameInfo
+	{
+		int wins;
+		int drops;
+	} data;
+
+
+	data.wins = 0;
+	data.drops = 0;
+
+	numWins=0; //DEBUG
 	while (numWins>0)
 	{
-		numWins = checkGame(gameBoard, &tileRng);
+		numWins = checkGame(STATE_BOUGHTGAME, gameBoard, jam, jamTriggerSwitch, &tileRng);
 		printf ("%i wins\n", numWins);
+		for (int i=1; i<7; i++) printf ("%i, ", jam[i]);
+				printf("\n");
+	
+		for (int i=1; i<7; i++) printf ("%i, ", jamTriggerSwitch[i]);
+		printf("\n");
+		data.wins += numWins;
+		data.drops ++;
+	}
+
+
+
+	printf ("End of game. Won %i in %i drops\n", data.wins, data.drops);
+
+	if (jamTriggerSwitch[0] > 10000)
+	{
+		printf("%i Jam Jars ready to trigger for the free spin \n", jamTriggerSwitch[0]);
+		numWins=1;
+
+		createGame(gameBoard, &tileRng );
+		printBoard(gameBoard);
+
+		while (numWins>0)
+		{
+			numWins = checkGame(STATE_FREEGAME, gameBoard, jam, jamTriggerSwitch, &tileRng);
+			printf ("%i wins\n", numWins);
+			for (int i=1; i<7; i++) printf ("%i, ", jam[i]);
+					printf("\n");
+		
+			for (int i=1; i<7; i++) printf ("%i, ", jamTriggerSwitch[i]);
+			printf("\n");
+						
+		}
+
+
 	}
 
 
